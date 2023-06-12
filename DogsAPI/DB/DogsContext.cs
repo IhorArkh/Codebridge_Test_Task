d272b2d4cda0ceac948cea5d19e9be4517ea56da
IHORS-PC\wasbo <wasbornindecember@gmail.com>
2023-06-12 15:31:17 +0300

    Sample of API project added, DB with 2 dogs created via migration

---
+++ DogsAPI/DB/DogsContext.cs
@@ -0,0 +1,20 @@
+﻿using DogsAPI.DB.Configuration;
+using DogsAPI.DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogsAPI.DB
+{
+    public class DogsContext : DbContext
+    {
+        public DbSet<Dog> Dogs { get; set; }
+
+        public DogsContext(DbContextOptions<DogsContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new DogConfiguration());
+        }
+    }
+}
---
+++ DogsAPI/DB/Models/Dog.cs
@@ -0,0 +1,13 @@
+﻿namespace DogsAPI.DB.Models
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public int TailLength { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
