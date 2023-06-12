a23217be4798e8a4bdbf67b640c0d780383e0c5a
IHORS-PC\wasbo <wasbornindecember@gmail.com>
2023-06-12 16:57:14 +0300

    Action that allows creating dogs ready

--- DogsAPI/Controllers/DogsController.cs
+++ DogsAPI/Controllers/DogsController.cs
@@ -1,5 +1,7 @@
 using DogsAPI.DB;
+using DogsAPI.DB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DogsAPI.Controllers
 {
@@ -20,11 +22,41 @@ namespace DogsAPI.Controllers
             return Ok("Dogs house service. Version 1.0.1");
         }
 
-        //[HttpGet("dogs")]
-        //public IActionResult GetDogs()
-        //{
-        //    var dogs = _dbContext.Dogs.ToList();
-        //    return Ok(dogs);
-        //}
+        [HttpPost("dog")]
+        public async Task<IActionResult> CreateDog(Dog dog)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingDog = await _dbContext.Dogs.FirstOrDefaultAsync(d => d.Name == dog.Name);
+                if (existingDog != null)
+                {
+                    return Conflict("A dog with the same name already exists.");
+                }
+
+                if (dog.TailLength < 0)
+                {
+                    return BadRequest("Tail length cannot be a negative number.");
+                }
+
+                if (dog.Weight < 0)
+                {
+                    return BadRequest("Tail weight cannot be a negative number.");
+                }
+
+                _dbContext.Dogs.Add(dog);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok("Dog created successfully.");
+            }
+
+            return BadRequest("Invalid dog data.");
+        }
+
+        [HttpGet("dogs")]
+        public IActionResult GetDogs()
+        {
+            var dogs = _dbContext.Dogs;
+            return Ok(dogs);
+        }
     }
 }
